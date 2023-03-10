7a98e64a2dfb1b38e93e2c162be19a4a1f8f1133
Ashkan Ganj <AshkanGanj@gmail.com>
2023-03-10 14:11:37 -0500

    add pretty qtable

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def main():
     # Simple learning curve
     plt.plot(reward_per_episode)
     # print(agentQ.q_table)
-    #pretty(agentQ.q_table)
+    pretty(agentQ.q_table)
     print("HeatMap : ")
     showHeatMap(environment, agentQ.heat_map)
     print("Policy :")
