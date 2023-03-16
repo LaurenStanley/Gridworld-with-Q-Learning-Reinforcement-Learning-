9fb12dbc770ab4a0c83cbfd5d102834ff024a93a
Ashkan Ganj <AshkanGanj@gmail.com>
2023-03-16 19:04:03 -0400

    add mean rewards

--- main.py
+++ main.py
@@ -3,93 +3,103 @@ import matplotlib.pyplot as plt
 from gridworld import GridWorld
 from q_agent import Q_Agent
 import time
-import os, math
+import os
+import math
 import sys
- 
+
 # How often to sample data for plotting
-sample_frequency = 0.25 # seconds
+sample_frequency = 0.25  # seconds
 
 # Method for calculating the decay rate of epsilon
-def get_epsilon(epsilon_initial,t, grid_size, flag, current_reward, max_time):
-    if flag: # based on time
+
+
+def get_epsilon(epsilon_initial, t, grid_size, flag, current_reward, max_time):
+    if flag:  # based on time
         time_remaining = max_time-t
         grid_epsilon = 1-np.exp(-np.sqrt(grid_size)/10)
         time_epsilon = 1-np.exp(-time_remaining/4)
         reward_epsilon = np.exp(-(current_reward+9)/7)
         epsilon = grid_epsilon*time_epsilon*reward_epsilon
         return epsilon
-    else: 
-        decay_rate = 0.99997/(1+ np.exp(-((grid_size)/1.4)))
-        return epsilon_initial* decay_rate
+    else:
+        decay_rate = 0.99997/(1 + np.exp(-((grid_size)/1.4)))
+        return epsilon_initial * decay_rate
 
 
 # Loop which keeps approaching the terminal state over time
-def play(environment, agent, pt4_flag = False, max_time =10, max_steps_per_episode=1000, learn=True, epsilon = 0.9, epsilon_decay = True):
+def play(environment, agent, pt4_flag=False, max_time=10, max_steps_per_episode=1000, learn=True, epsilon=0.9, epsilon_decay=True):
     print(epsilon_decay)
     #environment = GridWorld(filename)
     #environment.current_location = [(ind, environment.board[ind].index('S')) for ind in range(len(environment.board)) if 'S' in environment.board[ind]][0]
-    reward_per_episode = [] # Initialise performance log
+    reward_per_episode = []  # Initialise performance log
     init = time.time()
     epsilons = []
     mean_rewards = []
     cumulative_reward_array = []
-    time_steps = np.arange(init,max_time + init+sample_frequency,sample_frequency)
+    time_steps = np.arange(init, max_time + init +
+                           sample_frequency, sample_frequency)
     current_timestep_index = 1
     agent.epsilon = epsilon
-    sum_rewards = 0
+    sum_rewards = []
     trial = 0
     cumulative_reward_array_mean = -9
+
     while (time.time() - init < max_time):
-        cumulative_reward = 0 # Initialise values of each game
+        cumulative_reward = 0  # Initialise values of each game
         step = 0
         game_over = False
-        while step < max_steps_per_episode and game_over != True: # Run until max steps or until game is finished
+        while step < max_steps_per_episode and game_over != True:  # Run until max steps or until game is finished
             old_state = environment.current_location
-            action = agent.choose_action(environment.actions) 
+            action = agent.choose_action(environment.actions)
             reward = environment.make_step(action)
             new_state = environment.current_location
-            #print(agent.epsilon)
+            # print(agent.epsilon)
             if epsilon_decay == 'Decay':
-                # decay_rate = 0.99997/(1+ np.exp(-((environment.height*environment.width)/1.4)))
-                # agent.epsilon = agent.epsilon* decay_rate
-                agent.epsilon = get_epsilon(agent.epsilon,time.time() - init, environment.height * environment.width, pt4_flag, cumulative_reward_array_mean, max_time)
+                agent.epsilon = get_epsilon(agent.epsilon, time.time(
+                ) - init, environment.height * environment.width, pt4_flag, cumulative_reward_array_mean, max_time)
             elif epsilon_decay == 'Linear':
                 if agent.epsilon > 0.000005:
                     agent.epsilon = agent.epsilon - 0.000005
-                
-            if learn == True: # Update Q-values if learning is specified
+
+            if learn == True:  # Update Q-values if learning is specified
                 agent.learn(old_state, reward, new_state, action)
 
-            sum_rewards += reward
-            if reward > 9:
-                print(reward)
             cumulative_reward += reward
             step += 1
             agent.heat_map[new_state] = agent.heat_map[new_state] + 1
-            
+
             if environment.check_state() == 'TERMINAL':
                 if time.time() > time_steps[current_timestep_index]:
-                    cumulative_reward_array_mean = np.mean(cumulative_reward_array)
+                    cumulative_reward_array_mean = np.mean(
+                        cumulative_reward_array)
                     mean_rewards.append(cumulative_reward_array_mean)
                     epsilons.append(agent.epsilon)
                     cumulative_reward_array = []
                     current_timestep_index += 1
-                    trial +=1
                     
                 cumulative_reward_array.append(cumulative_reward)
+                sum_rewards.append(cumulative_reward)
+                trial += 1
                 game_over = True
                 environment = reset(environment)
 
-        #print(cumulative_reward)
-        reward_per_episode.append(cumulative_reward) # Append reward for current trial to performance log
-    avg_reward_per_trial = sum_rewards/trial
-    return reward_per_episode,avg_reward_per_trial, mean_rewards, epsilons # Return performance log
+
+        # print(cumulative_reward)
+        # Append reward for current trial to performance log
+        reward_per_episode.append(cumulative_reward)
+    print("tiral:",trial, "sum rewards:",sum(sum_rewards))
+    avg_reward_per_trial = sum(sum_rewards)/trial
+    # Return performance log
+    return reward_per_episode, avg_reward_per_trial, mean_rewards, epsilons
+
 
 def reset(environment):
     environment.grid = np.zeros((environment.height, environment.width))-1
     environment.find_terminal_states()
-    environment.current_location = [(ind, environment.board[ind].index('S')) for ind in range(len(environment.board)) if 'S' in environment.board[ind]][0]    
+    environment.current_location = [(ind, environment.board[ind].index(
+        'S')) for ind in range(len(environment.board)) if 'S' in environment.board[ind]][0]
     return environment
+
 
 def showHeatMap(environment, heatmap):
     total = 0
@@ -106,12 +116,13 @@ def showHeatMap(environment, heatmap):
                 out += 'X'.ljust(6) + ' | '
             elif val in environment.terminal_states:
                 out += str(environment.board[i]
-                            [j]).ljust(6) + ' | '
+                           [j]).ljust(6) + ' | '
             else:
                 out += str(round(100 *
-                            heatmap[(i, j)]/total, 2)).ljust(6) + ' | '
+                                 heatmap[(i, j)]/total, 2)).ljust(6) + ' | '
         print(out)
     print('-'*(9*len(environment.board[0])+1))
+
 
 def showPolicy(environment, d):
     for i in range(0, environment.height):
@@ -119,8 +130,8 @@ def showPolicy(environment, d):
         out = '| '
         for j in range(0, environment.width):
             token = " "
-            val = (i,j)
-            utes = d[i,j]
+            val = (i, j)
+            utes = d[i, j]
             action = max(utes, key=utes.get)
             if val in environment.barriers:
                 token = 'X'
@@ -135,23 +146,24 @@ def showPolicy(environment, d):
                     token = ">"
                 if action == "LEFT":
                     token = "<"
-                
+
             out += token.ljust(6) + ' | '
         print(out)
     print('-'*(9*environment.width+1))
 
+
 def main():
-    #Initialize
-    os.system('cls') 
+    # Initialize
+    os.system('cls')
     # Initialize environment and agent
-    
+
     filename = './test0.txt'
-    max_time = 10
+    max_time = 20
     per_action_reward = 0.1
     transition_success = 0.7
     pt4_flag = False
     ignore_time = False
-    
+
     #filename = sys.argv[1]
     #max_time = sys.argv[2]
     #per_action_reward = sys.argv[3]
@@ -161,24 +173,25 @@ def main():
     environment = GridWorld(filename, per_action_reward, transition_success)
     agentQ = Q_Agent(environment)
 
-    epsilons = [[1,'Decay']]
+    epsilons = [[1, 'Decay']]
     results = []
     epsilon_lists = []
     for epsilon in epsilons:
         # print(epsilon)
         # max time : how long the agent will explore the environment
-        reward_per_episode,avg_reward_per_trial, mean_rewards, epsilon_list = play(environment, agentQ,pt4_flag, max_time, epsilon = epsilon[0], epsilon_decay = epsilon[1])
+        reward_per_episode, avg_reward_per_trial, mean_rewards, epsilon_list = play(
+            environment, agentQ, pt4_flag, max_time, epsilon=epsilon[0], epsilon_decay=epsilon[1])
         # print(reward_per_episode)
         results.append(mean_rewards)
-        print("mean reward per trial:",avg_reward_per_trial)
+        print("mean reward per trial:", avg_reward_per_trial)
         epsilon_lists.append(epsilon_list)
     # Simple learning curve
     for result in results:
-        time = np.arange(0,len(result)*sample_frequency,sample_frequency)
-        plt.scatter(time,result)
-        plt.plot(time, result, '.r-') 
+        time = np.arange(0, len(result)*sample_frequency, sample_frequency)
+        plt.scatter(time, result)
+        plt.plot(time, result, '.r-')
 
-    #plt.scatter(range(0,len(mean_rewards)),mean_rewards[::-1])
+    # plt.scatter(range(0,len(mean_rewards)),mean_rewards[::-1])
     plt.xlabel("Time (s)")
     plt.ylabel("Average Reward")
     plt.legend(epsilons)
@@ -186,19 +199,21 @@ def main():
     plt.show()
 
     for epsilon_data in epsilon_lists:
-        time = np.arange(0,len(epsilon_data)*sample_frequency,sample_frequency)
+        time = np.arange(0, len(epsilon_data) *
+                         sample_frequency, sample_frequency)
         plt.scatter(time, epsilon_data)
-        plt.plot(time, epsilon_data, '.b-') 
+        plt.plot(time, epsilon_data, '.b-')
     plt.xlabel("Time (s)")
     plt.ylabel("Epsilon")
     plt.title('Epsilon over Time')
     plt.legend(epsilons)
     plt.show()
-    
+
     print("HeatMap : ")
     showHeatMap(environment, agentQ.heat_map)
     print("Policy :")
-    showPolicy(environment,agentQ.q_table)
-    
+    showPolicy(environment, agentQ.q_table)
+
+
 if __name__ == "__main__":
     main()
