9c721afbb732497f11407220558fd5c66062f581
Ashkan Ganj <AshkanGanj@gmail.com>
2023-03-16 17:16:03 -0400

    add mean reward per trial

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from gridworld import GridWorld
 from q_agent import Q_Agent
 import time
-import os
+import os, math
 import sys
  
 # How often to sample data for plotting
@@ -36,6 +36,8 @@ def play(environment, agent, pt4_flag = False, max_time =10, max_steps_per_episo
     time_steps = np.arange(init,max_time + init+sample_frequency,sample_frequency)
     current_timestep_index = 1
     agent.epsilon = epsilon
+    sum_rewards = 0
+    trial = 0
     cumulative_reward_array_mean = -9
     while (time.time() - init < max_time):
         cumulative_reward = 0 # Initialise values of each game
@@ -64,11 +66,13 @@ def play(environment, agent, pt4_flag = False, max_time =10, max_steps_per_episo
             
             if environment.check_state() == 'TERMINAL':
                 if time.time() > time_steps[current_timestep_index]:
+                    sum_rewards += cumulative_reward
                     cumulative_reward_array_mean = np.mean(cumulative_reward_array)
                     mean_rewards.append(cumulative_reward_array_mean)
                     epsilons.append(agent.epsilon)
                     cumulative_reward_array = []
                     current_timestep_index += 1
+                    trial +=1
                     
                 cumulative_reward_array.append(cumulative_reward)
                 game_over = True
@@ -76,8 +80,8 @@ def play(environment, agent, pt4_flag = False, max_time =10, max_steps_per_episo
 
         #print(cumulative_reward)
         reward_per_episode.append(cumulative_reward) # Append reward for current trial to performance log
-        
-    return reward_per_episode, mean_rewards, epsilons # Return performance log
+    avg_reward_per_trial = sum_rewards/trial
+    return reward_per_episode,avg_reward_per_trial, mean_rewards, epsilons # Return performance log
 
 def reset(environment):
     environment.grid = np.zeros((environment.height, environment.width))-1
@@ -140,7 +144,7 @@ def main():
     # Initialize environment and agent
     
     filename = './test0.txt'
-    max_time = 2
+    max_time = 20
     per_action_reward = 0.1
     transition_success = 0.7
     pt4_flag = False
@@ -152,20 +156,19 @@ def main():
     #transition_success = sys.argv[4]
     #ignore_time = sys.argv[5]
 
-    
     environment = GridWorld(filename, per_action_reward, transition_success)
     agentQ = Q_Agent(environment)
 
-    # epsilons = [[0.01, False],[0.1, False],[0.3, False],[0.99, False]]
-    #epsilons = [[0.9, 'Decay'],[0.9, 'Static'],[0.9,'Linear']]
     epsilons = [[1,'Decay']]
     results = []
     epsilon_lists = []
     for epsilon in epsilons:
         # print(epsilon)
         # max time : how long the agent will explore the environment
-        reward_per_episode, mean_rewards, epsilon_list = play(environment, agentQ,pt4_flag, max_time= 10, epsilon = epsilon[0], epsilon_decay = epsilon[1])
+        reward_per_episode,avg_reward_per_trial, mean_rewards, epsilon_list = play(environment, agentQ,pt4_flag, max_time, epsilon = epsilon[0], epsilon_decay = epsilon[1])
+        # print(reward_per_episode)
         results.append(mean_rewards)
+        print("mean reward per trial:",avg_reward_per_trial)
         epsilon_lists.append(epsilon_list)
     # Simple learning curve
     for result in results:
@@ -177,7 +180,7 @@ def main():
     plt.ylabel("Average Reward")
     plt.legend(epsilons)
     plt.title('Reward over Time')
-    plt.show()
+    # plt.show()
 
     for epsilon_data in epsilon_lists:
         time = np.arange(0,len(epsilon_data)*sample_frequency,sample_frequency)
@@ -186,12 +189,12 @@ def main():
     plt.ylabel("Epsilon")
     plt.title('Epsilon over Time')
     plt.legend(epsilons)
-    plt.show()
+    # plt.show()
     
     print("HeatMap : ")
     showHeatMap(environment, agentQ.heat_map)
     print("Policy :")
     showPolicy(environment,agentQ.q_table)
-
+    
 if __name__ == "__main__":
     main()
